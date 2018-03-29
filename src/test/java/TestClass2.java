9353a0c2458b83553ef493ca109adc565bcb8355
Kosinski <p.kosinski@accenture.com>
2018-03-29 09:23:11 +0200

    sada

---
+++ src/test/java/TestClass2.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+
+    @Test
+    public void test1(){
+        System.out.println(this.getClass().getName() + "test nr 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println(this.getClass().getName() + "test nr 2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println(this.getClass().getName() + "test nr 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println(this.getClass().getName() + "test nr 4");
+    }
+
+    @Test
+    public void test5(){
+        System.out.println(this.getClass().getName() + "test nr 5");
+    }
+
+
+
+}
