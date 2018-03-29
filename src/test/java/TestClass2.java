ea53c6e0e97b1884e3910144b1f9a6ad11bc8868
Kosinski <p.kosinski@accenture.com>
2018-03-29 10:21:37 +0200

    sada

--- src/test/java/TestClass2.java
+++ src/test/java/TestClass2.java
@@ -6,16 +6,21 @@ public class TestClass2 {
     @Test
     public void test1(){
         System.out.println(this.getClass().getName() + "test nr 1");
+        Assert.fail();
     }
 
     @Test
     public void test2(){
         System.out.println(this.getClass().getName() + "test nr 2");
+        Assert.fail();
+
     }
 
     @Test
     public void test3(){
         System.out.println(this.getClass().getName() + "test nr 3");
+        Assert.fail();
+
 
 
     }
@@ -23,6 +28,8 @@ public class TestClass2 {
     @Test
     public void test4(){
         System.out.println(this.getClass().getName() + "test nr 4");
+        Assert.fail();
+
     }
 
     @Test
