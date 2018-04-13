cac593ff2d8667b31b5ea7694ab2ac10d6051774
Kosinski <p.kosinski@accenture.com>
2018-04-13 11:46:40 +0200

    Sasdad

--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -2,12 +2,43 @@ import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class TestClass1 {
+
+    private Properties properties = new Properties();
+    private InputStream inputStream;
+
+
 
     @Test
     public void test1(){
         System.out.println(this.getClass().getName() + "test nr 1");
-        System.out.println("${name}");
+
+        try {
+            inputStream = new FileInputStream("src/main/resources/user.properties");
+            properties.load(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        String username = properties.getProperty("name");
+
+        System.out.println(username);
+
+
+        String valueFromMaven = System.getProperty("propertyName");
+        System.out.println(valueFromMaven);
+
+
+
     }
 
     @Test
