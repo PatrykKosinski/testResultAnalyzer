d5afd1e4268f9931bd8999f5684eee6effe44b34
Kosinski <p.kosinski@accenture.com>
2018-04-18 16:40:35 +0200

    asdasda

---
+++ src/test/java/TestClass5.java
@@ -0,0 +1,43 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestClass5 {
+
+    @Test
+    public void test1(){
+        System.out.println(this.getClass().getName() + "test nr 1");
+
+    }
+
+    @Test
+    public void test2(){
+        System.out.println(this.getClass().getName() + "test nr 2");
+        Assert.fail();
+
+    }
+
+    @Test
+    public void test3(){
+        System.out.println(this.getClass().getName() + "test nr 3");
+
+
+
+
+    }
+
+    @Test
+    public void test4(){
+        System.out.println(this.getClass().getName() + "test nr 4");
+
+        Assert.fail();
+    }
+
+    @Test
+    public void test5(){
+        System.out.println(this.getClass().getName() + "test nr 5");
+
+    }
+
+
+
+}
